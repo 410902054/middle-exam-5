20f391be9de0ae5aef1a4cd338ebcca4f434e4e5
410902054 <71751283+410902054@users.noreply.github.com>
2020-11-18 15:27:29 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("peko")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+basic.showString("XD")
+basic.clearScreen()
+basic.forever(function () {
+	
+})
